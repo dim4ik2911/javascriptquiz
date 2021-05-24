88ec51197cef2e4f10ebb09c8d12347d29c5819f
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-05-24 20:32:21 +0100

    javascript function added

--- main.js
+++ main.js
@@ -1,9 +1,42 @@
-const question1 = document.getElementById("question1");
-question1.addEventListener("click", (event) => {
+const questions1 = document.getElementById("question1");
+const questions2 = document.getElementById("question2");
+const questions3 = document.getElementById("question3");
+const questions4 = document.getElementById("question4");
+const questions5 = document.getElementById("question5");
+const questions6 = document.getElementById("question6");
+const questions7 = document.getElementById("question7");
+const questions8 = document.getElementById("question8");
+const questions9 = document.getElementById("question9");
+const questions10 = document.getElementById("question10");
+const result = document.getElementById("result");
+
+const checkAnswer = (event) => {
   const target = event.target;
+  const rightArr = [];
   if (target.classList.contains("right-answer")) {
-    target.style.border = ".3rem solid #00B806";
+    target.style.border = ".5rem solid #00B806";
+    target.style.backgroundColor = "#0ACF48";
+
+    rightArr.push(target.innerHTML);
+    console.log(rightArr);
+    result.innerHTML = `Your score is ${rightArr.length} / 10`;
+  } else if (target.classList.contains("wrong-answer")) {
+    target.style.border = ".5rem solid #D60000";
+    target.style.backgroundColor = "#F00201";
   } else {
-    target.style.border = ".3rem solid #EB0027";
+    return;
   }
-});
+
+  result.innerHTML = `Your score is ${rightArr.length} / 10`;
+};
+
+question1.addEventListener("click", checkAnswer, { once: true });
+question2.addEventListener("click", checkAnswer, { once: true });
+question3.addEventListener("click", checkAnswer, { once: true });
+question4.addEventListener("click", checkAnswer, { once: true });
+question5.addEventListener("click", checkAnswer, { once: true });
+question6.addEventListener("click", checkAnswer, { once: true });
+question7.addEventListener("click", checkAnswer, { once: true });
+question8.addEventListener("click", checkAnswer, { once: true });
+question9.addEventListener("click", checkAnswer, { once: true });
+question10.addEventListener("click", checkAnswer, { once: true });
